c4cb7702d7bcc5b7938f55cc5997c8b95d60c060
RichardWatterson <87286040+RichardWatterson@users.noreply.github.com>
2021-07-11 19:15:59 -0500

    Add files via upload

---
+++ Serializacion_JSON/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacion_JSON
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string genero { get; set; }
+        public int edad { get; set; }
+        public DateTime nacimiento { get; set; }
+    }
+}
---
+++ Serializacion_JSON/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace Serializacion_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona yo = new Persona
+            {
+                nombre = "Richard",
+                apellidos = "Watterson",
+                genero = "Masculino",
+                edad = 23,
+                nacimiento = DateTime.Parse("1997-10-10")
+            };
+            Console.WriteLine("Serialización"); 
+            Console.WriteLine("La persona {0} {1} serializada a JSON es:\n{2}",yo.nombre,yo.apellidos, JsonSerializer.Serialize<Persona>(yo));
+
+            
+            using (WebClient wc = new WebClient()){
+                string jsonStr = wc.DownloadString("https://copypaste.codes/raw/vbarxpjk6d");
+                yo = Deserializer(jsonStr);
+                if (yo != null)
+                {
+                    Console.WriteLine("Deserialización");
+                    Console.WriteLine("Mi nombre es: {0} {1}, tengo {2} años\nsoy del genero {3} y nací el {4}",yo.nombre,yo.apellidos,yo.edad,yo.genero,yo.nacimiento);
+                }
+            }
+        }
+        static Persona Deserializer(string jsonString)
+        {
+            Persona temp;
+            try
+            {
+                JsonDocument.Parse(jsonString, options: default);
+                return JsonSerializer.Deserialize<Persona>(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error con el JSON String");
+                return null;
+            }
+        }
+    }
+}
